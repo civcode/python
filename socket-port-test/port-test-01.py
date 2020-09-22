5cd9a0d2dcc4d9337d2b61ad48f424622f18827a
chris <ivi3001@yahoo.de>
2020-09-22 19:27:29 +0200

    added json output

--- socket-port-test/port-test-01.py
+++ socket-port-test/port-test-01.py
@@ -1,3 +1,4 @@
+import json
 import os
 import signal
 
@@ -13,19 +14,22 @@ def probe_port(port):
     if output.find('successful') != 0:
         #print('success!')
         print(output.splitlines()[0])
+        return output.splitlines()[0]
 
 def run_test():
     #port = 334
     port_start = 20
     port_stop = 1000
     timeout = 0.2
+    output = []
     for port in range(port_start, port_stop):
         signal.signal(signal.SIGALRM, handler)
         #signal.alarm(1)
         signal.setitimer(signal.ITIMER_REAL, timeout)
 
         try:
-            probe_port(port)
+            out = probe_port(port)
+            output.append(out)
         except Exception as exc:
             #print(exc)
             #print('Port ' + str(port) + ' is blocked.')
@@ -34,6 +38,12 @@ def run_test():
         #signal.alarm(0)
         #signal.setitimer(signal.ITIMER_REAL, 0)
         #signal.setitimer(0)
+    print(output)
+    #json_format = json.dumps(output)
+    #print(json_format)
+    with open('data.json', 'w', encoding='utf-8') as f:
+        json.dump(output, f)
+        #json.dump(json_format, f)
 
 run_test()
 #stream = os.popen('echo Retuned output')
