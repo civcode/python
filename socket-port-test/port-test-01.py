294a1fa065095d52ae51ba71fddd0f65f037c555
chris <ivi3001@yahoo.de>
2020-09-25 15:56:20 +0200

    added timeout to wget

--- socket-port-test/port-test-01.py
+++ socket-port-test/port-test-01.py
@@ -1,4 +1,5 @@
 import json
+import math
 import os
 import signal
 import threading
@@ -7,9 +8,11 @@ import time
 def handler(signum, frame):
     raise Exception("timeout")
 
-def probe_port(port):
-    #print(str(port))
-    stream = os.popen('wget -qO- portquiz.net:' + str(port))
+def probe_port(port, timeout):
+    timeout = (math.ceil(timeout))
+    #print('probe_port()')
+    #print('wget --timeout ' + str(timeout) + ' -qO- portquiz.net:' + str(port))
+    stream = os.popen('wget --timeout ' + str(timeout) + ' -qO- portquiz.net:' + str(port))
     output = stream.read()
     #print(output)
     signal.setitimer(signal.ITIMER_REAL, 0)
@@ -24,18 +27,22 @@ def run_test(port_start, port_stop, timout):
     #port_stop = 1000
     #timeout = 0.2
     output = []
+    counter = 0
     for port in range(port_start, port_stop):
         signal.signal(signal.SIGALRM, handler)
         #signal.alarm(1)
         signal.setitimer(signal.ITIMER_REAL, timeout)
+        counter += 1
+        if counter%1 == 0:
+            print(port_start + counter, end='\r')
 
         try:
-            out = probe_port(port)
+            out = probe_port(port, timeout)
             output.append(out)
             time.sleep(timeout)
         except Exception as exc:
             #print(exc)
-            print('Port ' + str(port) + ' is blocked.')
+            #print('Port ' + str(port) + ' is blocked.')
             continue
             
         #time.sleep(2)
@@ -49,15 +56,17 @@ def run_test(port_start, port_stop, timout):
         json.dump(output, f)
 
 if __name__ == "__main__":
-    #timeout = 0.2
-    timeout = 1.5
-    
+    timeout = 0.5
+    #timeout = 1.0
+    port_start = 38800
+    port_stop = 65536 
+
     #a = threading.Thread(target=run_test, args=(20, 50, timeout))
     #a.start()
     #a.join()
 
     #run_test(20, 50, timeout)
-    run_test(1, 100, timeout)
+    run_test(port_start, port_stop, timeout)
 
 #stream = os.popen('echo Retuned output')
 #output = stream.read()
